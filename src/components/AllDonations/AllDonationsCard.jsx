4fa17befa39ed0ffe47bbff1de9f36171cd747f7
ashik-08 <ashikurrahman.8october@gmail.com>
2023-09-27 17:26:33 +0600

    feat: added see all button for the donation page only when there are more than 4 donations it will be visible

--- src/components/AllDonations/AllDonationsCard.jsx
+++ src/components/AllDonations/AllDonationsCard.jsx
@@ -4,6 +4,7 @@ import DonationCard from "./DonationCard";
 const AllDonationsCard = () => {
   const [allDonations, setAllDonations] = useState([]);
   const [notFound, setNotFound] = useState(false);
+  const [dataLength, setDataLength] = useState(4);
 
   useEffect(() => {
     const donations = JSON.parse(localStorage.getItem("donation"));
@@ -16,11 +17,32 @@ const AllDonationsCard = () => {
   }, []);
 
   return (
-    <div>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-20 mb-36">
-        {allDonations.map((donation) => (
-          <DonationCard key={donation.id} donation={donation}></DonationCard>
-        ))}
+    <div className="mt-14 mb-20 lg:mt-20 lg:mb-36">
+        {/* donation card items */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        {allDonations &&
+          allDonations
+            .slice(0, dataLength)
+            .map((donation) => (
+              <DonationCard
+                key={donation.id}
+                donation={donation}
+              ></DonationCard>
+            ))}
+      </div>
+      {/* see all button */}
+      {/* only shows when there are more than 4 donations */}
+      <div
+        className={
+          dataLength === allDonations.length || allDonations.length <= 4 ? "hidden " : "text-center mt-10"
+        }
+      >
+        <button
+          onClick={() => setDataLength(allDonations.length)}
+          className="bg-[#009444] text-white text-lg font-semibold px-5 py-2.5 rounded-lg"
+        >
+          See All
+        </button>
       </div>
     </div>
   );
